06fbba3eb2ec1536b556bd88bdc7e5987d0ce72f
Dan Varela <danmar.dev.ph@tripleonetech.net>
2023-12-31 05:24:28 +0800

    feat(theme): add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main>hello</main>
+  return <div className="flex justify-center items-center h-screen">
+    <Button>hello</Button>
+  </div>
 }
